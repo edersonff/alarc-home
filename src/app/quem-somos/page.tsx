e9c1c50b61f1b068c734190c92007731ca61c1df
edersonff <=>
2024-01-28 18:29:48 -0300

    feat: quem-somos page

---
+++ src/app/quem-somos/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import Button from "@/components/Button";
+import OutlinedButton from "@/components/Button/Outlined";
+import { Info, Typo } from "@/components/Typo";
+import { quemSomos } from "@/info";
+import ContentLayout from "@/layout/content";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+export default function QuemSomos() {
+  const { push } = useRouter();
+  return (
+    <ContentLayout info="quemSomos">
+      <div className="content flex flex-col gap-14 my-[72px]">
+        <Typo typo="paragraph" className="lg:w-2/3 xl:w-2/3">
+          <Info info="quemSomos" text="text" />
+        </Typo>
+        <div className="flex gap-7">
+          <Button onClick={() => push(quemSomos.redirect)}>Saiba Mais</Button>
+          <OutlinedButton onClick={() => push("/")}>Voltar</OutlinedButton>
+        </div>
+        <img
+          className="absolute right-0 w-[35%] h-[70vh] object-scale-down object-right xl:block lg:block hidden"
+          src={quemSomos.image}
+          alt="Quem Somos"
+        />
+      </div>
+    </ContentLayout>
+  );
+}
