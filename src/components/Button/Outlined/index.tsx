b230402c0d84339e438fa645671224738333c54a
edersonff <=>
2024-01-28 18:28:50 -0300

    feat: button outlined

---
+++ src/components/Button/Outlined/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function OutlinedButton({
+  className,
+  ...props
+}: JSX.IntrinsicElements["button"]) {
+  return (
+    <button
+      className={
+        "py-4 px-8 border-2 min-w-44 rounded-full font-medium text-xl border-primary text-primary hover:bg-primary/15 transition-all active:bg-opacity-85 " +
+        className
+      }
+      {...props}
+    />
+  );
+}
