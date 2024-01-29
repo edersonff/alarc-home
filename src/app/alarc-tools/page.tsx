8413d94983d06bd9014a853e6933417aeb25207b
edersonff <=>
2024-01-28 22:37:59 -0300

    feat: alarc-tools page

---
+++ src/app/alarc-tools/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import Button from "@/components/Button";
+import OutlinedButton from "@/components/Button/Outlined";
+import { Info, Typo } from "@/components/Typo";
+import { tools } from "@/info";
+import ContentLayout from "@/layout/content";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+export default function HeroTools() {
+  const { push } = useRouter();
+  return (
+    <ContentLayout info="tools">
+      <div className="content flex flex-col gap-14 my-[72px]">
+        <Typo typo="paragraph" className="lg:w-2/3 xl:w-2/3">
+          <Info info="tools" text="text" />
+        </Typo>
+        <div className="flex gap-7">
+          <Button onClick={() => push(tools.redirect)}>Saiba Mais</Button>
+          <OutlinedButton onClick={() => push("/")}>Voltar</OutlinedButton>
+        </div>
+        <img
+          className="absolute right-0 w-[35%] h-[60vh] object-scale-down object-right xl:block lg:block hidden"
+          src={tools.image}
+          alt="Quem Somos"
+        />
+      </div>
+    </ContentLayout>
+  );
+}
