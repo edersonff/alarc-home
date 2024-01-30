f03e6afed110046e376fa727aa443c839e6b0bd2
edersonff <=>
2024-01-30 00:06:23 -0300

    feat: test images domains

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  images: {
+    domains: ["images.pexels.com", "www.cnnbrasil.com.br", "grupoahora.net.br"],
+  },
 };
 
 export default nextConfig;
