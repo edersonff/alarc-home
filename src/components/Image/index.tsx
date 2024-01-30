d643a1bdd8ab4e666180a5a3b69279e49067e1f3
edersonff <=>
2024-01-30 00:07:26 -0300

    feat: Loading Image component

---
+++ src/components/Image/index.tsx
@@ -0,0 +1,26 @@
+import NaviveImage, { ImageProps } from "next/image";
+import React, { useMemo, useState } from "react";
+import Skeleton from "../Skeleton";
+
+export default function ImageLoading(props: ImageProps) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const isFill = useMemo(() => {
+    return props.layout === "fill";
+  }, [props.layout]);
+
+  return (
+    <>
+      {isLoading && (
+        <Skeleton
+          style={{
+            width: props.width || "100%",
+            height: props.height || "100%",
+          }}
+          className={`${isFill ? "absolute top-0 left-0" : ""}`}
+        />
+      )}
+      <NaviveImage {...props} onLoad={() => setIsLoading(false)} />
+    </>
+  );
+}
