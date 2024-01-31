855239b6e8ed76f4638b33a3c68a67c1a260ef97
edersonff <=>
2024-01-31 16:35:22 -0300

    fix: admin layout

--- src/layout/admin/index.tsx
+++ src/layout/admin/index.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Header from "@/components/Admin/Header";
+import Navbar from "@/components/Admin/Navbar";
 import { useAuthStore } from "@/store/auth";
 import { useRouter } from "next/navigation";
 import React from "react";
@@ -19,8 +21,16 @@ export default function AdminLayout({
   }
 
   return (
-    <div>
-      <>{children}</>
+    <div className="w-full h-[100vh]">
+      <div className="pt-7 flex w-full h-full">
+        <Navbar />
+        <div className="w-full h-full flex flex-col">
+          <Header />
+          <div className="bg-green-600/5 h-full p-[30px] rounded-tl-[25px]">
+            {children}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
