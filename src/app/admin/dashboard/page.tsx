6ea7eddfa6117d17a0c0f95310e097e052b55a5c
edersonff <=>
2024-01-31 13:18:45 -0300

    feat: admin dash

---
+++ src/app/admin/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import AdminLayout from "@/layout/admin";
+import React from "react";
+
+export default function Dashboard() {
+  return (
+    <AdminLayout>
+      <>a</>
+    </AdminLayout>
+  );
+}
