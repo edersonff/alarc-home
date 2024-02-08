7adb251c707709788ad8a0141e46d783622a9f41
edersonff <=>
2024-02-07 23:57:33 -0300

    feat: posts edit and delete

--- src/app/admin/posts/page.tsx
+++ src/app/admin/posts/page.tsx
@@ -23,32 +23,40 @@ import ReactQuill from "react-quill";
 import Image from "next/image";
 import { postsService } from "@/services/posts";
 import dynamic from "next/dynamic";
+import Modal from "@/components/Modal";
+import { useErrorStore } from "@/store/error";
 
 export default function Posts() {
-  const [post, setPost] = React.useState<string | null>(null);
+  const [updatePost, setUpdatePost] = useState<string | null>(null);
+  const [deletePost, setDeletePost] = useState<string | null>(null);
   const posts = useInfoStore((state) => state.posts);
 
   const editPost = useMemo(() => {
-    const findPost = posts.find((p) => p.slug === post);
+    const findPost = posts.find((p) => p.slug === updatePost);
 
     if (findPost) {
       return findPost;
     }
 
     return null as any;
-  }, [post, posts]);
+  }, [updatePost, posts]);
 
   function onCloseDrawer() {
-    setPost(null);
+    setUpdatePost(null);
   }
 
   return (
     <AdminLayout>
       <PostEdit
-        slug={post}
-        open={post !== null}
+        slug={updatePost}
+        open={updatePost !== null}
         post={editPost}
         onClose={onCloseDrawer}
+      />
+      <DeleteModal
+        slug={deletePost}
+        open={deletePost !== null}
+        onClose={() => setDeletePost(null)}
       />
       <div className="flex flex-gap w-full gap-main">
         <div className="flex-1 flex flex-col gap-main">
@@ -64,7 +72,7 @@ export default function Posts() {
               </div>
               <div className="flex gap-[15px] items-center">
                 <Button
-                  href="/admin/blog/create"
+                  href="/admin/posts/create"
                   className="text-lg py-[12px]"
                   target=""
                 >
@@ -78,12 +86,15 @@ export default function Posts() {
                 <div key={post.slug} className="relative w-full">
                   <div className="absolute right-1 top-1 z-30 flex gap-[15px]">
                     <OutlinedButton
-                      onClick={() => setPost(post.slug)}
+                      onClick={() => setUpdatePost(post.slug)}
                       className="min-w-max text-xs py-[10px]"
                     >
                       Editar
                     </OutlinedButton>
-                    <OutlinedButton className="min-w-max text-xs py-[10px] border-red-600 text-red-600 hover:bg-red-600/15">
+                    <OutlinedButton
+                      onClick={() => setDeletePost(post.slug)}
+                      className="min-w-max text-xs py-[10px] border-red-600 text-red-600 hover:bg-red-600/15"
+                    >
                       Excluir
                     </OutlinedButton>
                   </div>
@@ -109,7 +120,9 @@ function PostEdit({
   slug: string | null;
   onClose: () => void;
 }) {
-  const [imageURL, setImageURL] = useState<string | null>(post?.image);
+  const pushError = useErrorStore((state) => state.pushError);
+
+  const [imageURL, setImageURL] = useState<string | null>(null);
   const title = useRef<HTMLInputElement>(null);
   const slug = useRef<HTMLInputElement>(null);
   const owner = useRef<HTMLInputElement>(null);
@@ -118,8 +131,26 @@ function PostEdit({
   const text = useRef<ReactQuill>(null);
   const image = useRef<HTMLInputElement>(null);
 
-  const ReactQuill: any = useMemo(
-    () => dynamic(() => import("react-quill"), { ssr: false }),
+  const ReactQuill = useMemo(
+    () =>
+      dynamic(
+        async () => {
+          const { default: RQ } = await import("react-quill");
+
+          type Props = {
+            forwardedRef: any;
+          } & ReactQuill["props"];
+
+          const quill = ({ forwardedRef, ...props }: Props) => (
+            <RQ ref={forwardedRef} {...props} />
+          );
+
+          return quill;
+        },
+        {
+          ssr: false,
+        }
+      ),
     []
   );
 
@@ -145,7 +176,7 @@ function PostEdit({
     const ownerValue = owner.current?.value;
     const tagsValue = tags.current?.value;
     const dateValue = date.current?.value;
-    const textValue = text.current?.getEditor().getText();
+    const textValue = text.current?.getEditor().root.innerHTML;
     const imageValue = image.current?.files?.[0];
 
     const isValid =
@@ -167,8 +198,25 @@ function PostEdit({
     if (imageValue) formData.append("image", imageValue);
     if (ownerValue) formData.append("owner", ownerValue);
 
-    await postsService.update(selectedSlug, formData);
+    await postsService
+      .update(selectedSlug, formData)
+      .then(() => {
+        window.location.reload();
+      })
+      .catch((err) => {
+        pushError({
+          message: err?.response?.data?.message || "Erro ao salvar post.",
+          status: err.status,
+        });
+      });
   }
+
+  if (!post)
+    return (
+      <Drawer open={open} onClose={onClose}>
+        <></>
+      </Drawer>
+    );
 
   return (
     <Drawer open={open} onClose={onClose}>
@@ -179,28 +227,33 @@ function PostEdit({
             <EditInput
               innerRef={title}
               label="Título"
-              defaultValue={post?.title}
+              defaultValue={post.title}
               required
             />
-            <EditInput innerRef={slug} label="Slug" defaultValue={post?.slug} />
+            <EditInput
+              innerRef={slug}
+              label="Slug"
+              required
+              defaultValue={post.slug}
+            />
             <EditInput
               innerRef={owner}
               label="Dono"
-              defaultValue={post?.owner}
+              defaultValue={post.owner}
             />
             <EditInput
               label="Categorias"
               innerRef={tags}
-              defaultValue={post?.tags.join(", ")}
+              defaultValue={post.tags.join(", ")}
               required
             />
             <EditInput
               innerRef={date}
               label="Data de Publicação"
-              defaultValue={post?.date}
+              defaultValue={post.date}
               required
             />
-            <ReactQuill ref={text} theme="snow" value={post?.text} />;
+            <ReactQuill forwardedRef={text} theme="snow" value={post.text} />;
             <div className="flex flex-col gap-[2px]">
               <label
                 htmlFor="upload-photo"
@@ -212,11 +265,11 @@ function PostEdit({
                   </p>
                 </div>
                 <Image
-                  src={imageURL || "/static/img/placeholder.png"}
+                  src={imageURL || post.image || "/static/img/placeholder.png"}
                   layout="fill"
                   objectFit="cover"
                   className="group-hover:grayscale transition-all duration-300"
-                  alt={post?.title}
+                  alt={post.title}
                 />
               </label>
               <input
@@ -256,5 +309,61 @@ function EditInput({
         className="w-full border border-gray-300 p-2 rounded-md"
       />
     </div>
+  );
+}
+
+function DeleteModal({
+  slug,
+  onClose,
+  open,
+}: {
+  slug: string | null;
+  open: boolean;
+  onClose: () => void;
+}) {
+  const pushError = useErrorStore((state) => state.pushError);
+
+  function onDelete() {
+    if (slug) {
+      postsService
+        .delete(slug)
+        .then(() => {
+          window.location.reload();
+        })
+        .catch((err) => {
+          pushError({
+            message: err?.response?.data?.message || "Erro ao deletar post.",
+            status: err.status,
+          });
+        });
+    }
+  }
+
+  return (
+    <Modal open={open} onClose={onClose}>
+      <div className="w-full min-w-[500px] px-2 pt-2">
+        <h2 className="text-xl font-medium mb-2">
+          Tem certeza que deseja excluir?
+        </h2>
+        <p className="text-gray-600">Você não poderá desfazer essa ação.</p>
+        <div className="flex justify-end gap-4 mt-10">
+          <button
+            onClick={onClose}
+            className="rounded-full text-neutral-500 border-2 border-neutral-500 hover:bg-neutral-500/15 transition-all px-6 font-bold py-[12px]"
+          >
+            Cancelar
+          </button>
+          <Button
+            style={{
+              fontSize: "16px",
+            }}
+            className="bg-red-600 py-[12px]"
+            onClick={onDelete}
+          >
+            Excluir
+          </Button>
+        </div>
+      </div>
+    </Modal>
   );
 }
