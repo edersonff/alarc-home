977d904af04d3bbfbd5402b2c3973fe7c2288ef8
edersonff <=>
2024-02-12 16:48:09 -0300

    fix: tabs persist and sync auth store

--- src/store/auth/index.ts
+++ src/store/auth/index.ts
@@ -1,20 +1,20 @@
 import { create } from "zustand";
-import { persist, createJSONStorage } from "zustand/middleware";
+import { persistNSync } from "persist-and-sync";
 
 export type AuthStore = {
   isLogged: boolean;
   setLogged: (logged: boolean) => void;
 };
 
-const adminStore = persist<AuthStore>(
+const authStore = persistNSync<AuthStore>(
   (set, get) => ({
     isLogged: false,
     setLogged: (logged) => set({ isLogged: logged }),
   }),
   {
-    name: "admin-store",
-    storage: createJSONStorage(() => sessionStorage),
+    name: "auth-store",
+    storage: "localStorage",
   }
 );
 
-export const useAuthStore = create(adminStore);
+export const useAuthStore = create(authStore);
