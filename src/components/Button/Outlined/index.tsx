796b2fbe3f63956575fcbbfe45a68d3b8a6e5d8d
edersonff <=>
2024-02-12 16:47:18 -0300

    feat: innerRef in outlined button component

--- src/components/Button/Outlined/index.tsx
+++ src/components/Button/Outlined/index.tsx
@@ -2,10 +2,14 @@ import React from "react";
 
 export default function OutlinedButton({
   className,
+  innerRef,
   ...props
-}: JSX.IntrinsicElements["button"]) {
+}: {
+  innerRef?: React.RefObject<HTMLButtonElement>;
+} & JSX.IntrinsicElements["button"]) {
   return (
     <button
+      ref={innerRef}
       className={
         className +
         " py-4 px-8 center border-2 min-w-44 rounded-full font-medium text-xl border-primary text-primary hover:bg-primary/15 transition-all active:bg-opacity-85 "
