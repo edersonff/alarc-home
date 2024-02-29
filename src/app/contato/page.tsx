97e33485b720dab556efad19009401613a952f8b
edersonff <=>
2024-02-29 19:06:25 -0300

    feat: show up component/effect

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -15,6 +15,7 @@ import Lottie, { LottieRef } from "lottie-react";
 import emailAnim from "@/../public/static/lottie/email.json";
 import Link from "next/link";
 import Footer from "@/components/Footer";
+import Show from "@/components/Show";
 
 export default function Contato() {
   const contato = useInfoStore((state) => state.contato);
@@ -79,49 +80,53 @@ export default function Contato() {
       </div>
       <div className="w-full my-[230px]">
         <div className="content w-full flex flex-col gap-48">
-          <CTA
-            title="Venha nos conhecer"
-            subtitle="Nosso endereço"
-            link={{
-              label: "Vinhedo, São Paulo 13280168, BR",
-              href: "https://maps.app.goo.gl/y4QbqLAiceuuVnpt8",
-            }}
-            image={
-              <iframe
-                className="w-full h-full absolute top-0 left-0"
-                loading="lazy"
-                allowFullScreen
-                referrerPolicy="no-referrer-when-downgrade"
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d117486.80135974068!2d-47.07295625420295!3d-23.04349805682956!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cf32972381d79d%3A0x1dd8779027018146!2sVinhedo%2C%20SP%2C%2013280-000!5e0!3m2!1spt-BR!2sbr!4v1707251754335!5m2!1spt-BR!2sbr"
-              />
-            }
-            orientation="left"
-          >
-            Est libero exercitationem occaecati ducimus sit corrupti. Eos
-            asperiores qui porro facere quis velit. Iure magnam cupiditate et
-            minima dolores iste. Autem debitis vel porro itaque voluptatem.
-          </CTA>
-          <CTA
-            title="Mande mensagem"
-            subtitle="Nosso Whatsapp"
-            link={{
-              label: "Whatsapp - (47) 9 99625 9340",
-              href: "https://wa.me/479996259340",
-            }}
-            image={
-              <Image
-                src="/images/banner/whatsapp.jpg"
-                alt="whatsapp phone app"
-                layout="fill"
-                className="object-cover undraggable unselectable"
-              />
-            }
-            orientation="right"
-          >
-            Est libero exercitationem occaecati ducimus sit corrupti. Eos
-            asperiores qui porro facere quis velit. Iure magnam cupiditate et
-            minima dolores iste. Autem debitis vel porro itaque voluptatem.
-          </CTA>
+          <Show>
+            <CTA
+              title="Venha nos conhecer"
+              subtitle="Nosso endereço"
+              link={{
+                label: "Vinhedo, São Paulo 13280168, BR",
+                href: "https://maps.app.goo.gl/y4QbqLAiceuuVnpt8",
+              }}
+              image={
+                <iframe
+                  className="w-full h-full absolute top-0 left-0"
+                  loading="lazy"
+                  allowFullScreen
+                  referrerPolicy="no-referrer-when-downgrade"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d117486.80135974068!2d-47.07295625420295!3d-23.04349805682956!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cf32972381d79d%3A0x1dd8779027018146!2sVinhedo%2C%20SP%2C%2013280-000!5e0!3m2!1spt-BR!2sbr!4v1707251754335!5m2!1spt-BR!2sbr"
+                />
+              }
+              orientation="left"
+            >
+              Est libero exercitationem occaecati ducimus sit corrupti. Eos
+              asperiores qui porro facere quis velit. Iure magnam cupiditate et
+              minima dolores iste. Autem debitis vel porro itaque voluptatem.
+            </CTA>
+          </Show>
+          <Show>
+            <CTA
+              title="Mande mensagem"
+              subtitle="Nosso Whatsapp"
+              link={{
+                label: "Whatsapp - (47) 9 99625 9340",
+                href: "https://wa.me/479996259340",
+              }}
+              image={
+                <Image
+                  src="/images/banner/whatsapp.jpg"
+                  alt="whatsapp phone app"
+                  layout="fill"
+                  className="object-cover undraggable unselectable"
+                />
+              }
+              orientation="right"
+            >
+              Est libero exercitationem occaecati ducimus sit corrupti. Eos
+              asperiores qui porro facere quis velit. Iure magnam cupiditate et
+              minima dolores iste. Autem debitis vel porro itaque voluptatem.
+            </CTA>
+          </Show>
         </div>
       </div>
       <div className="xl-lg:min-h-[621px] w-full relative" id="forms">
--- src/components/Blocks/index.tsx
+++ src/components/Blocks/index.tsx
@@ -8,6 +8,7 @@ import TouchRipple from "@material-ui/core/ButtonBase/TouchRipple";
 import Link from "next/link";
 import { FaCirclePlus } from "react-icons/fa6";
 import { useInfoStore } from "@/store/info";
+import Show from "../Show";
 
 export default function Blocks() {
   const {
@@ -35,109 +36,125 @@ export default function Blocks() {
       <div className="content">
         <div className="flex gap-main columns-4 w-full min-h-[60vh] xl-lg:flex-row flex-col">
           <div className="flex-1 gap-main flex flex-col">
-            <Block
-              className="bg-neutral-200 text-black h-44"
-              href={nossosServicos.href}
-              plus="text-primary"
-            >
-              <div className="w-full">
-                <Typo typo="block" className="mb-3">
-                  <Info info="nossosServicos" text="label" />
-                </Typo>
-              </div>
-            </Block>
-            <Block
-              className="bg-secondary flex-1 text-white"
-              href={academia.href}
-              target="_blank"
-            >
-              <div className="w-full">
-                <Typo typo="block" className="mb-3">
-                  <Info info="academia" text="label" />
-                </Typo>
-                <BsArrowRightCircle className="text-white text-[54px]" />
-              </div>
-            </Block>
+            <Show delay={0}>
+              <Block
+                className="bg-neutral-200 text-black h-44"
+                href={nossosServicos.href}
+                plus="text-primary"
+              >
+                <div className="w-full">
+                  <Typo typo="block" className="mb-3">
+                    <Info info="nossosServicos" text="label" />
+                  </Typo>
+                </div>
+              </Block>
+            </Show>
+            <Show className="flex-1" delay={0.4}>
+              <Block
+                className="bg-secondary text-white"
+                href={academia.href}
+                target="_blank"
+              >
+                <div className="w-full">
+                  <Typo typo="block" className="mb-3">
+                    <Info info="academia" text="label" />
+                  </Typo>
+                  <BsArrowRightCircle className="text-white text-[54px]" />
+                </div>
+              </Block>
+            </Show>
           </div>
 
           <div className="flex-1 gap-main flex flex-col">
-            <Block
-              className="bg-primary flex-1 text-white"
-              href={quemSomos.href}
-              plus="text-white"
-            >
-              <div className="w-full">
-                <Typo typo="block" className="mb-3">
-                  <Info info="quemSomos" text="label" />
-                </Typo>
-              </div>
-            </Block>
-            <Block
-              className="bg-neutral-200 text-black flex-1"
-              href={tools.href}
-              plus="text-primary"
-            >
-              <div className="w-full">
-                <Typo typo="block" className="mb-3">
-                  <Info info="tools" text="label" />
-                </Typo>
-              </div>
-            </Block>
+            <Show delay={0.1} className="flex-1">
+              <Block
+                className="bg-primary text-white"
+                href={quemSomos.href}
+                plus="text-white"
+              >
+                <div className="w-full">
+                  <Typo typo="block" className="mb-3">
+                    <Info info="quemSomos" text="label" />
+                  </Typo>
+                </div>
+              </Block>
+            </Show>
+            <Show delay={0.5} className="flex-1">
+              <Block
+                className="bg-neutral-200 text-black"
+                href={tools.href}
+                plus="text-primary"
+              >
+                <div className="w-full">
+                  <Typo typo="block" className="mb-3">
+                    <Info info="tools" text="label" />
+                  </Typo>
+                </div>
+              </Block>
+            </Show>
           </div>
 
           <div className="flex-1 gap-main flex flex-col">
-            <Block
-              className="bg-neutral-200 flex-1 text-black"
-              href={nossosClientes.href}
-              plus="text-primary"
-            >
-              <div className="w-full">
-                <Typo typo="block" className="mb-3">
-                  <Info info="nossosClientes" text="label" />
-                </Typo>
-              </div>
-            </Block>
-            <Block
-              className="bg-secondary text-white flex-1"
-              href={canaisAtuacao.href}
-              plus="text-white"
-            >
-              <div className="w-full">
-                <Typo typo="block" className="mb-3">
-                  <Info info="canaisAtuacao" text="label" />
-                </Typo>
-              </div>
-            </Block>
+            <Show delay={0.2} className="flex-1">
+              <Block
+                className="bg-neutral-200 text-black"
+                href={nossosClientes.href}
+                plus="text-primary"
+              >
+                <div className="w-full">
+                  <Typo typo="block" className="mb-3">
+                    <Info info="nossosClientes" text="label" />
+                  </Typo>
+                </div>
+              </Block>
+            </Show>
+            <Show delay={0.6} className="flex-1">
+              <Block
+                className="bg-secondary text-white"
+                href={canaisAtuacao.href}
+                plus="text-white"
+              >
+                <div className="w-full">
+                  <Typo typo="block" className="mb-3">
+                    <Info info="canaisAtuacao" text="label" />
+                  </Typo>
+                </div>
+              </Block>
+            </Show>
           </div>
 
           <div className="flex-1 gap-main flex flex-col">
-            <Block
-              className="bg-primary text-white h-44"
-              href={noticias.href}
-              plus="text-white"
-            >
-              <div className="w-full">
-                <Typo typo="block" className="mb-3">
-                  <Info info="noticias" text="label" />
-                </Typo>
-              </div>
-            </Block>
-            <Block href={contato.href} className="bg-black flex-1 text-white">
-              <div className="w-full">
-                <Typo typo="block" className="mb-3">
-                  <Info info="contato" text="label" />
-                </Typo>
-              </div>
+            <Show delay={0.3}>
+              <Block
+                className="bg-primary text-white"
+                href={noticias.href}
+                plus="text-white"
+              >
+                <div className="w-full">
+                  <Typo typo="block" className="mb-3">
+                    <Info info="noticias" text="label" />
+                  </Typo>
+                </div>
+              </Block>
+            </Show>
+            <Show className="flex-1" delay={0.7}>
+              <Block href={contato.href} className="bg-black flex-1 text-white">
+                <div className="w-full">
+                  <Typo typo="block" className="mb-3">
+                    <Info info="contato" text="label" />
+                  </Typo>
+                </div>
 
-              <Image
-                src="/images/block/about.png"
-                alt="Logo"
-                layout="fill"
-                objectFit="cover"
-                loading="eager"
-                className="absolute top-0 left-0 -z-10 unselectable undraggable"
-              />
-            </Block>
+                <Image
+                  src="/images/block/about.png"
+                  alt="Logo"
+                  layout="fill"
+                  objectFit="cover"
+                  loading="eager"
+                  className="absolute top-0 left-0 -z-10 unselectable undraggable"
+                />
+              </Block>
+            </Show>
           </div>
         </div>
       </div>
@@ -180,7 +197,7 @@ export function Block({
       onMouseLeave={onRippleStop}
       onMouseUp={onRippleStop}
       className={
-        "flex flex-col justify-end items-center p-6 overflow-hidden rounded-[17px] relative z-10 unselectable undraggable min-h-52 cursor-pointer " +
+        "flex flex-col h-full justify-end items-center p-6 overflow-hidden rounded-[17px] relative z-10 unselectable undraggable min-h-52 cursor-pointer " +
         className
       }
     >
