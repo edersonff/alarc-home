84aca677a6a1b8aeb37158d50ee5badfc25729fa
edersonff <=>
2024-03-07 14:52:13 -0300

    feat: service blocks in alarc-tools

--- src/app/alarc-tools/page.tsx
+++ src/app/alarc-tools/page.tsx
@@ -2,11 +2,15 @@
 
 import Button from "@/components/Button";
 import OutlinedButton from "@/components/Button/Outlined";
+import ServiceBlock from "@/components/Service";
 import { Info, Typo } from "@/components/Typo";
 import ContentLayout from "@/layout/content";
 import { useInfoStore } from "@/store/info";
 import { useRouter } from "next/navigation";
 import React from "react";
+import { FaAd, FaBoxOpen } from "react-icons/fa";
+import { IoPricetags } from "react-icons/io5";
+import { TbReportAnalytics } from "react-icons/tb";
 
 export default function HeroTools() {
   const tools = useInfoStore((state) => state.tools);
@@ -14,14 +18,57 @@ export default function HeroTools() {
   return (
     <ContentLayout info="tools">
       <div className="content flex flex-col gap-14 my-[72px]">
-        <Typo typo="paragraph" className="xl-lg:w-2/3">
-          <Info info="tools" text="text" />
-        </Typo>
-        <img
-          className="xl-lg:absolute xl-lg:w-[35%] w-full right-0 h-auto object-right relative"
-          src={tools.image}
-          alt="Tools Image"
-        />
+        <div>
+          <div className="relative">
+            <Typo typo="paragraph" className="xl-lg:w-2/3">
+              <Info info="tools" text="text" />
+            </Typo>
+
+            <img
+              className="xl-lg:absolute xl-lg:w-[40%] w-full right-0 bottom-0 h-auto object-right relative pointer-events-none"
+              src={tools.image}
+              alt="Tools Image"
+            />
+          </div>
+
+          <br />
+
+          <div className="flex flex-col gap-main my-20">
+            <div className="flex xl-lg:flex-row flex-col gap-main">
+              <ServiceBlock
+                title="Precificação do Mercado Livre "
+                Icon={IoPricetags}
+                url="/contato"
+              >
+                Controle de preço de estoque do Mercado Livre
+              </ServiceBlock>
+              <ServiceBlock
+                title="Relatórios"
+                Icon={TbReportAnalytics}
+                url="/contato"
+              >
+                Relatórios de vendas detalhados por canal
+              </ServiceBlock>
+            </div>
+            <div className="flex xl-lg:flex-row flex-col gap-main">
+              <ServiceBlock
+                title="Automatização para carga"
+                Icon={FaBoxOpen}
+                url="/contato"
+              >
+                Automatização de carga FULL Mercado Livre
+              </ServiceBlock>
+              <ServiceBlock
+                title="Automatização de anuncios"
+                Icon={FaAd}
+                url="/contato"
+              >
+                Automatização de ADS Mercado Livre
+              </ServiceBlock>
+            </div>
+          </div>
+        </div>
+
         <div className="flex gap-main xl-lg:mt-24">
           <Button onClick={() => push(tools.redirect)}>Saiba Mais</Button>
           <OutlinedButton onClick={() => push("/")}>Voltar</OutlinedButton>
