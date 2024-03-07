d2097deb82a7ea11313196126ee1fe3a293876b0
edersonff <=>
2024-03-07 14:51:03 -0300

    fix: remove persist-and-sync package

--- src/store/auth/index.ts
+++ src/store/auth/index.ts
@@ -1,20 +1,25 @@
 import { create } from "zustand";
-import { persistNSync } from "persist-and-sync";
+import { share, isSupported } from "shared-zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 export type AuthStore = {
   isLogged: boolean;
   setLogged: (logged: boolean) => void;
 };
 
-const authStore = persistNSync<AuthStore>(
+const authStore = persist<AuthStore>(
   (set, get) => ({
     isLogged: false,
     setLogged: (logged) => set({ isLogged: logged }),
   }),
   {
     name: "auth-store",
-    storage: "localStorage",
+    storage: createJSONStorage(() => sessionStorage),
   }
 );
 
 export const useAuthStore = create(authStore);
+
+if ("BroadcastChannel" in globalThis && isSupported()) {
+  share("auth-store", useAuthStore);
+}
